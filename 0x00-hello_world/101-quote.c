972d8deee00b22182b2461ab1f52167ef9ec4283
stephenvov <stephenbonsu65@gmail.com>
2023-07-13 23:38:05 -0700

    done with project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description:
+ * Return:0 (success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
