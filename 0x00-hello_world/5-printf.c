b61726b3dab4c4da3bfedb5da3b971d28843bb16
stephenvov <stephenbonsu65@gmail.com>
2023-07-13 19:11:33 -0700

    3 to 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* print Programming is like building a multilingual puzzle */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
