d3d86f4945c339b159d0e2788b674e3e344536fc
stephenvov <stephenbonsu65@gmail.com>
2023-07-17 12:54:34 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: a-z
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for (int i=0;i>10;i++;)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
