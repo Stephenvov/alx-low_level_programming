2a14ee6e84b4ebebee5d17b58bcf9513c7e04669
stephenvov <stephenbonsu65@gmail.com>
2023-07-17 23:41:52 -0700

    10-two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,13 @@ int main(void)
 
 	for (i = 0; i < 90; i++)
 	{
-		if (i < 9)
+		putchar('0' + (i/10));
+		putchar ('0' + (i%10));
+		if (i != 89)
 		{
-			putchar (i + '0');
-		}
-		else
-		{
-			putchar ('0');
-			putchar (i + '0');
-		}
-		if (i == 89)
 			putchar (',');
+			putchar (' ');
+		}
 	}
 	putchar('\n');
 	return (0);
