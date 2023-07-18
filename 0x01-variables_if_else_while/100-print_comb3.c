1b18e04d69bc3eb7dd3b59e1ce165ba6b4a25880
stephenvov <stephenbonsu65@gmail.com>
2023-07-17 17:54:17 -0700

    9. Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: Write a program that prints all possible combinations
+ * of single-digit numbers.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 90; i++)
+	{
+		if (i < 9)
+		{
+			putchar (i + '0');
+		}
+		else
+		{
+			putchar ('0');
+			putchar (i + '0');
+		}
+		putchar (", ");
+	}
+	putchar('\n');
+	return (0);
+}
