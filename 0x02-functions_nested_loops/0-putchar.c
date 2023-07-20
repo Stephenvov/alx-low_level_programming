7048e071ce1d62f203b19daa5f5f6bd9239a0dcb
stephenvov <stephenbonsu65@gmail.com>
2023-07-19 18:31:08 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,7 @@
  * Description: display _putchar
  * Return: 0 (success)
  */
+int _putchar(char x);
 int main(void)
 {
 	_putchar('_');
@@ -14,5 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
