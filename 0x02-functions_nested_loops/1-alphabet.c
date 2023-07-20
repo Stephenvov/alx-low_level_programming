d871f80410777642e88f615f6104d82ffbdd8d10
stephenvov <stephenbonsu65@gmail.com>
2023-07-19 23:15:39 -0700

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Main - Entry
+ * Description: list of a-z
+ * Return: success 0
+ */
+int main(void)
+{
+	int i;
+	char x = 'a';
+
+	while ( x == '{')
+	{
+		_putchar(x);
+		x++;
+	}
+	return (0);
+}
