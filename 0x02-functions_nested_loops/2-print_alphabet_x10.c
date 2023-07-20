22430b35c6b794d50cbd7def5a4dfb7a1ed4de3c
stephenvov <stephenbonsu65@gmail.com>
2023-07-20 05:32:49 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry
+ * Description: printing a - z
+ * Return: 0 (success)
+ */
+void void print_alphabet_x10(void)
+{
+	char x = 'a';
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		while (x != '{')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+	}
+}
