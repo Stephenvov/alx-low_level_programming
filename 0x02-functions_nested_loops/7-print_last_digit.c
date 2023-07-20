351bc1d3d430d4ad6b256f4d1aea35908a093862
stephenvov <stephenbonsu65@gmail.com>
2023-07-20 13:21:09 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/**
+/** 
  * print_last_digit - Entry
  * Description: print_last_digit
  * Return: 0
  */
 int print_last_digit(int x)
 {
-	_putchar(x%10);
-	return (x%10);
+	int y = x%10;
+	_putchar('0' + y);
+	return (y);
 }
