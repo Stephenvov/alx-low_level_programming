135453ad777edb0c184c05d16c7734a856029ebe
stephenvov <stephenbonsu65@gmail.com>
2023-07-22 10:14:04 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Entry
+ * Description: printing from 0 - 9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
