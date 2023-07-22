33c4ecec100e1880fbe44cb2f6d61661dd4c36a2
stephenvov <stephenbonsu65@gmail.com>
2023-07-22 15:04:32 -0700

    11-print to 90

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,53 +3,45 @@
  * print_to_98 - Entry
  * Description: print from n to 98
  * @n: number from function
- * Return: 0;
  */
 void print_to_98(int n)
 {
-	if (n < 99)
+	while (n > 99)
 	{
-		while (n < 99)
-		{
-			if (n > -10 && n < 10)
-			{
-				_putchar(n + '0');
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n++;
-		}
+		_putchar(n / 10 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+		n--;
 	}
-	else if (n > 98)
+	while (n <= 99)
 	{
-		while (n >= 98)
+		if (n > -99 && n < -9)
 		{
-			if (n > 99)
-			{
-				_putchar(n / 100 + '0');
-				_putchar((n % 100) / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n--;
+			_putchar('-');
+			_putchar(-n / 10 + '0');
+			_putchar(-n % 10 + '0');
 		}
+		else if (n > -10 && n < 0)
+		{
+			_putchar(-n + '0');
+		}
+		else if (n >= 0 && n <= 9)
+		{
+			_putchar(n + '0');
+		}
+		else
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
 	}
 	_putchar('\n');
 }
